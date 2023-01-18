4cd452c0e00083e3b389356f1485c19370db5d5c
Maha Lakshmi <maha422004@gmail.com>
2023-01-18 19:18:20 +0530

    combobox using .NET framework(C# programming)

---
+++ combo_box_demo/WinFormsApp3/Form1.cs
@@ -0,0 +1,48 @@
+//programs for combobox control
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Graphics g = base.CreateGraphics();
+            Pen mypen = new Pen(Color.Blue);
+            g.Clear(Color.White);
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    g.DrawEllipse(mypen, 50, 50, 150, 150);
+                    break;
+                case 1:
+                    g.DrawEllipse(mypen, 50, 75, 120, 150);
+                    break;
+                case 2:
+                    g.DrawRectangle(mypen, 50, 50, 150, 150);
+                    break;
+            }
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("circle");
+            comboBox1.Items.Add("ellipse");
+            comboBox1.Items.Add("square");
+
+        }
+    }
+}
