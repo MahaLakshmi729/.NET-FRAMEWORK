5fcf3a676b80dff3335de5deb1317e00b0da9138
Maha Lakshmi <maha422004@gmail.com>
2023-01-22 22:43:11 +0530

    function_example

---
+++ functionexample/functionexample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+using System.Threading.Tasks;
+namespace functionexample
+{
+    class program
+    {
+        public void show(string message)
+        {
+            Console.WriteLine("hello" + message);
+        }
+        static void Main(string[] args)
+        {
+            program program = new program();
+            program.show("world");
+        }
+    }
+}
