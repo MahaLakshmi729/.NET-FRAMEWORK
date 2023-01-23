03da8052f0ec7e15bfcb05ee0b0b173df5df576b
Maha Lakshmi <maha422004@gmail.com>
2023-01-23 22:45:12 +0530

    single_dimensional_array

---
+++ single_dimensional_array/single_dimensional_array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+namespace singledimensionalarrays
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[3];
+            int totalmarks = 0;
+            Console.Write("enter the marks in subject A: ");
+            marks[0] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the marks in subject B: ");
+            marks[1] = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("enter the marks in subject C: ");
+            marks[2] = Convert.ToInt32(Console.ReadLine());
+            totalmarks = marks[0]+ marks[1]+ marks[2];
+            Console.Write("total marks: " + totalmarks);
+            Console.ReadLine();
+        }
+    }
+}
