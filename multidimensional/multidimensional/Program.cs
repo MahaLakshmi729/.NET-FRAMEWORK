5d5249163640605b70679558431ece742a60a268
Maha Lakshmi <maha422004@gmail.com>
2023-01-23 22:43:48 +0530

    multidimensional

---
+++ multidimensional/multidimensional/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace multidimensional
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[2, 2];
+            Console.Write("enter value for the matrix[0,0] element: ");
+            matrix[0, 0] = System.Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter value for the matrix[0,1] element: ");
+            matrix[0, 1] = System.Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter value for the matrix[1,0] element: ");
+            matrix[1, 0] = System.Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter value for the matrix[1,1] element: ");
+            matrix[1, 1] = System.Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the generated matrix is: ");
+            Console.WriteLine("\t\t\t" + matrix[0,0]+" " + matrix[0,1]);
+            Console.WriteLine("\t\t\t" + matrix[1,0] + " " + matrix[1,1]);
+            Console.ReadLine();
+        }
+
+    }
+}
