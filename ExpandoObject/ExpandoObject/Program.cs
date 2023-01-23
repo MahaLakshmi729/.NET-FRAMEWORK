14789a49125da3cccbeb7dd83efd4478bb3a9f96
Maha Lakshmi <maha422004@gmail.com>
2023-01-23 17:38:13 +0530

    ExpandoObject

---
+++ ExpandoObject/ExpandoObject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+using System.Threading.Tasks;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        dynamic son, father;
+        son = new ExpandoObject();
+        son.Name = "son";
+        son.Age = 18;
+        father = new ExpandoObject();
+        father.Name = "father";
+        father.Age = 45;
+        father.Children = 2;
+        WriteFamily(father);
+        WriteFamily(son);
+        Console.WriteLine("\n");
+        Console.WriteLine("Press enter to quit...");
+        Console.Read();
+    }
+    private static void WriteFamily(dynamic member)
+    {
+        Console.WriteLine("{0} is {1} years old.", member.Name, member.Age);
+    }
+}
