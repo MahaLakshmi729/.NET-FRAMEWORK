338f21bf2437a0387d75db5469966bcc80b13b9c
Maha Lakshmi <maha422004@gmail.com>
2023-01-25 18:49:46 +0530

    class

---
+++ class/class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace creatingobjects
+{
+    class class1
+    {
+        private int num1 = 24;
+        private int num2 = 36;
+        public void average()
+        {
+            int av = (num1 + num2) / 2;
+            Console.WriteLine("average: " + av);
+
+        }
+        class program
+        {
+            static void Main(string[] args)
+            {
+                class1 obj=new class1();
+                obj.average();
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
+
