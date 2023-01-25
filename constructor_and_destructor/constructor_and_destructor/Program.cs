406f69efc22cd881dd1b06922188a48a13c99422
Maha Lakshmi <maha422004@gmail.com>
2023-01-25 19:21:31 +0530

    constructor_and_destructors

---
+++ constructor_and_destructor/constructor_and_destructor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace constructorsanddestructors
+{
+    class rectangle
+    {
+        int length,width;
+        public rectangle(int x, int y)
+        {
+            length = x;
+            width = y;
+        }
+        rectangle()
+        {
+            Console.WriteLine("finalizing rectangle..");
+            Console.ReadLine();
+        }
+        public void area()
+        {
+            Console.WriteLine("area of the triangle:" + length * width);
+        }
+     }
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            rectangle rect = new rectangle(30, 20);
+            rect.area();
+        }
+    }
+}
+
