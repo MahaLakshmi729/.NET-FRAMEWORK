cc05db467a8b27de091e236fee76078ebe44c7e8
Maha Lakshmi <maha422004@gmail.com>
2023-01-25 18:50:36 +0530

    no_parameter_returntype

---
+++ no_parameter_returntype/no_parameter_returntype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace functionexample
+{
+    class program
+    {
+        public void show()
+        {
+            Console.WriteLine("the is non parameterized function");
+        }
+        static void Main(string[] args) 
+        {
+        program program= new program();
+            program.show();
+        }
+    }
+}
