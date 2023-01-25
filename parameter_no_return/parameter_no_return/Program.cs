3f607f68ca3493716508c7cc8ef842e9d9271a3c
Maha Lakshmi <maha422004@gmail.com>
2023-01-25 18:51:22 +0530

    parameter_no_returntype

---
+++ parameter_no_return/parameter_no_return/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace FunctionExample
+{
+    class Program
+    {
+        
+        public void Show(string message)
+        {
+            Console.WriteLine("Hello " + message);
+            
+        }
+        
+        static void Main(string[] args)
+        {
+            Program program = new Program(); 
+            program.Show("Mohammed anas"); 
+        }
+    }
+}
