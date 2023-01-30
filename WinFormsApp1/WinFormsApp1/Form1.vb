dc6e73a84bc06e39aa03c51e55185ac0135a9815
Maha Lakshmi <maha422004@gmail.com>
2023-01-30 18:32:48 +0530

    projecting_list

---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim words() As String = {"red", "blue", "white", "yellow", "green"}
+        For Each w In words
+            ListBox1.Items.Add(w.ToString())
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim words(4) As String
+        For index = 0 To 4
+            words(index) = ListBox1.Items(index)
+        Next
+        Dim query = From word In words Select word.Substring(0, 1)
+        For Each letters In query
+            ListBox2.Items.Add(letters.ToString())
+        Next
+
+    End Sub
+End Class
