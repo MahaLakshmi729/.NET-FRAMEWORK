0ac0885bcb51b00f29b9c3b7a6c5cc7d6c5ce83c
Maha Lakshmi <maha422004@gmail.com>
2023-02-03 21:35:27 +0530

    method_overloading

---
+++ method overloading/method overloading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace methodoverriding
+{
+    public class animal
+    {
+        public virtual void sleep()
+        {
+            Console.WriteLine("the animal is sleeping....");
+        }
+    }
+    public class dog : animal 
+    {
+        public override void sleep()
+        {
+            Console.WriteLine($"the dog is sleeping.....");
+        }
+
+    }
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            animal a = new animal();
+            a.sleep();
+            dog d = new dog();
+            d.sleep();
+            Console.ReadLine();
+        }
+    }
+}
