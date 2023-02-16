eb7ab4f8b3ccf244c3c6995c4d662e6aea5a2151
Maha Lakshmi <111877117+MahaLakshmi729@users.noreply.github.com>
2023-02-16 18:08:06 +0530

    dynamic_type

---
+++ dynamic type/dynamic type/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace dynamicType
+{
+    class Program
+    {
+        static dynamic dyn = 5.56;
+        public void method1(string str,int b)
+        {
+            Console.WriteLine(str);
+            Console.WriteLine(b);
+        }
+        static void Main(string[] args)
+        {
+            Program p= new Program();
+            p.method1("hello", 45);
+            dyn = "welcome";
+            p.method1(dyn, 56);
+            Console.Read();
+        }
+    }
+}
